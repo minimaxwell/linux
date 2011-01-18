e6b4dd72ebab342175ab18c9aa3a2357b128b5e9
Florent TRINH-THAI <florent.trinh-thai@c-s.fr>
2011-01-18 14:30:57 +0000

    - Reprise du driver NAND pour MCR3000 1G (debut de chantier)

--- drivers/mtd/nand/mcr3000.c
+++ drivers/mtd/nand/mcr3000.c
@@ -19,9 +19,18 @@
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/nand.h>
 #include <linux/mtd/partitions.h>
+#include <linux/of_platform.h>
 #include <asm/io.h>
 #include <asm/cpm1.h>
 #include <asm/fs_pd.h>
+
+/*
+ * Driver identification
+ */
+#define DRV_NAME	"mcr3000"
+#define DRV_VERSION	"1.0"
+#define DRV_AUTHOR	"Florent TRINH THAI"
+#define DRV_DESC	"MCR3000 on-chip NAND FLash Controller Driver"
 
 /*
  * MTD structure for MCR3000 board
@@ -29,6 +38,11 @@
 static struct mtd_info *mcr3000_mtd = NULL;
 void __iomem *mcr3000_fio_base;
 void __iomem *mcr3000_cpld_status;
+
+/*
+ * GPIO declaration
+ */
+#define MCR3000_NB_GPIO 3
 
 /*
  * Values specific to the MCR3000 board (used with PPC8xx processor)
@@ -45,12 +59,68 @@ static struct mtd_partition partition_info[] = {
 	 .size		= (12*1024*1024),
 	 .offset	= 0},
 	{.name		= "Part 2 JFFS2 (application)",
-	 .size		= (20*1024*1024),
-	 .offset	= (12*1024*1024)}
+	 .size		= (MTDPART_SIZ_FULL),
+	 .offset	= (MTDPART_OFS_APPEND)}
 };
 #endif		//#ifdef CONFIG_MTD_PARTITIONS
 
 #define NUM_PARTITIONS 2
+
+
+///* Select the chip by setting nCE to low */
+//#define NAND_CTL_SETNCE		1
+///* Deselect the chip by setting nCE to high */
+//#define NAND_CTL_CLRNCE		2
+///* Select the command latch by setting CLE to high */
+//#define NAND_CTL_SETCLE		3
+///* Deselect the command latch by setting CLE to low */
+//#define NAND_CTL_CLRCLE		4
+///* Select the address latch by setting ALE to high */
+//#define NAND_CTL_SETALE		5
+///* Deselect the address latch by setting ALE to low */
+//#define NAND_CTL_CLRALE		6
+
+//static void mcr3000_hwcontrol(struct mtd_info *mtd, int cmd)
+//{
+//	register struct nand_chip *tthis = mtd->priv;
+//
+//	switch (cmd) {
+//
+//	case NAND_CTL_SETCLE:
+//		tthis->IO_ADDR_W = p_nand + MEM_STNAND_CMD;
+//		break;
+//
+//	case NAND_CTL_CLRCLE:
+//		tthis->IO_ADDR_W = p_nand + MEM_STNAND_DATA;
+//		break;
+//
+//	case NAND_CTL_SETALE:
+//		tthis->IO_ADDR_W = p_nand + MEM_STNAND_ADDR;
+//		break;
+//
+//	case NAND_CTL_CLRALE:
+//		tthis->IO_ADDR_W = p_nand + MEM_STNAND_DATA;
+//		/* FIXME: Nobody knows why this is necessary,
+//		 * but it works only that way */
+//		udelay(1);
+//		break;
+//
+//	case NAND_CTL_SETNCE:
+//		/* assert (force assert) chip enable */
+//		au_writel((1 << (4 + NAND_CS)), MEM_STNDCTL);
+//		break;
+//
+//	case NAND_CTL_CLRNCE:
+//		/* deassert chip enable */
+//		au_writel(0, MEM_STNDCTL);
+//		break;
+//	}
+//
+//	tthis->IO_ADDR_R = tthis->IO_ADDR_W;
+//
+//	/* Drain the writebuffer */
+//	au_sync();
+//}
 
 /* 
  *	hardware specific access to control-lines
@@ -117,14 +187,37 @@ const char *part_probes[] = { "cmdlinepart", NULL };
 #endif
 
 /*
- * Main initialization routine
+ * mcr3000_remove
  */
-int __init mcr3000_init (void)
+static int __devexit mcr3000_remove(struct of_device *ofdev)
+{
+	/* Release resources, unregister device */
+	nand_release(mcr3000_mtd);
+
+	/* unmap physical address */
+	iounmap(mcr3000_fio_base);
+	iounmap(mcr3000_cpld_status);
+
+	/* Free the MTD device structure */
+	kfree(mcr3000_mtd);
+	
+	return 0;
+}
+
+/*
+ * mcr3000_probe
+ */
+static int __devinit mcr3000_probe(struct of_device *ofdev, const struct of_device_id *ofid)
 {
 	int mtd_parts_nb = 0;
 	struct nand_chip *this;
 	const char *part_type = 0;
 	struct mtd_partition *mtd_parts = 0;
+
+
+
+	printk ("Probing NAND FLASH.\n");
+
 
 	/* Allocate memory for MTD device structure and private data */
 	mcr3000_mtd = kmalloc (sizeof(struct mtd_info) + sizeof (struct nand_chip), GFP_KERNEL);
@@ -158,6 +251,22 @@ int __init mcr3000_init (void)
 	mcr3000_mtd->priv  = this;
 	mcr3000_mtd->owner = THIS_MODULE;
 	
+	/* GPIO definition */
+//	if (ngpios < MCR3000_NB_GPIO) {
+//		dev_err(dev, "missing GPIO definition in device tree\n");
+//		_Result = -EINVAL;
+//		goto ERREUR;
+//	}
+//	for (i = 0; i < M_DRV_ALM_NB_GPIO; i++) {
+//		int gpio = ldb_gpio_init(np, dev, i, dir[i]);
+//		if (gpio == -1) {
+//			dev_err(dev, "problem with GPIO init\n");
+//			_Result = -EINVAL;
+//			goto ERREUR;
+//		}
+//		stAlmSuivi.mGpio[i] = gpio;
+//	}	
+
 	/*
 	 * Set GPIO Port D control register so that the pins are configured
 	 * to be outputs for controlling the NAND flash.
@@ -202,30 +311,65 @@ int __init mcr3000_init (void)
 	/* Register the partitions */
 	printk(KERN_NOTICE "Using %s partition definition\n", part_type);
 	add_mtd_partitions(mcr3000_mtd, mtd_parts, mtd_parts_nb);
-
+	
+	
+	
+	printk ("NAND FLASH probed.\n");
+	
+	
+	
 	/* Return happy */
 	return 0;
 }
-module_init(mcr3000_init);
+
+static const struct of_device_id mcr3000_match[] =
+{
+	{
+		.compatible   = "s3k,mcr3000-nand",
+	},
+	{},
+};
+
+MODULE_DEVICE_TABLE(of, mcr3000_match);
+
+/*
+ * driver device registration
+ */
+static struct of_platform_driver mcr3000_driver = {
+	.probe		= mcr3000_probe,
+	.remove		= __devexit_p(mcr3000_remove),
+	.driver		=
+	{
+		.name		= DRV_NAME,
+		.owner		= THIS_MODULE,
+		.of_match_table	= mcr3000_match,
+	},
+};
+
+/*
+ * Main initialization routine
+ */
+int __init mcr3000_init (void)
+{
+
+	printk ("mcr3000_init\n");
+
+
+	return of_register_platform_driver(&mcr3000_driver);
+}
 
 /*
  * Clean up routine
  */
 static void __exit mcr3000_cleanup(void)
 {
-	/* Release resources, unregister device */
-	nand_release(mcr3000_mtd);
-
-	/* unmap physical address */
-	iounmap(mcr3000_fio_base);
-	iounmap(mcr3000_cpld_status);
-
-	/* Free the MTD device structure */
-	kfree(mcr3000_mtd);
+	of_unregister_platform_driver(&mcr3000_driver);
 }
 
+module_init(mcr3000_init);
 module_exit(mcr3000_cleanup);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Florent TRINH THAI");
-MODULE_DESCRIPTION("Board-specific glue layer for NAND flash on MCR3000 board");
+MODULE_AUTHOR(DRV_AUTHOR);
+MODULE_DESCRIPTION(DRV_DESC);
+MODULE_ALIAS("platform:" DRV_NAME);
