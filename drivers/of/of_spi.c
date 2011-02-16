555675cd4ab0b49e3c88765e32f6ab0764e0387e
Christophe LEROY <christophe.leroy@c-s.fr>
2011-02-16 11:46:04 +0000

    introduction d'un nouveau mode sur bus SPI pour mettre des coups d'horlge supplémentaires après le passage inactif du chipselect

--- drivers/of/of_spi.c
+++ drivers/of/of_spi.c
@@ -63,6 +63,8 @@ void of_register_spi_devices(struct spi_master *master, struct device_node *np)
 			spi->mode |= SPI_CPOL;
 		if (of_find_property(nc, "spi-cs-high", NULL))
 			spi->mode |= SPI_CS_HIGH;
+		if (of_find_property(nc, "spi-troll", NULL))
+			spi->mode |= SPI_TROLL;
 
 		/* Device speed */
 		prop = of_get_property(nc, "spi-max-frequency", &len);
