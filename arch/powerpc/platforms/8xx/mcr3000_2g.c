c357754d2ab2e3039d436d50e7b083746fece6d4
Christophe LEROY <christophe.leroy@c-s.fr>
2011-09-09 09:28:54 +0000

    struct fpgaf dans un .h

--- arch/powerpc/platforms/8xx/mcr3000_2g.c
+++ arch/powerpc/platforms/8xx/mcr3000_2g.c
@@ -25,37 +25,10 @@
 
 #include <sysdev/simple_gpio.h>
 
+#include <saf3000/fpgaf.h>
+
 #include "mpc8xx.h"
 #include "mcr3000_2g.h"
-
-struct fpgaf {
-	u16 ident;
-	u16 version;
-	u16 res1[6];
-	u16 reset;
-	u16 res2[7];
-	u16 it_mask;
-	u16 it_pend;
-	u16 it_ack;
-	u16 it_ctr;
-	u16 res3[4];
-	u16 alrm_in;
-	u16 alrm_out;
-	u16 res4[6];
-	u16 fonc_gen;
-	u16 addr;
-	u16 res5[6];
-	u16 pll_ctr;
-	u16 pll_status;
-	u16 pll_src;
-	u16 res6[5];
-	u16 net_ref;
-	u16 etat_ref;
-	u16 res7[6];
-	u16 syn_h110;
-	u16 res8[7];
-	u16 test;
-};
 
 /*
  * Controlleur d'IRQ du FPGA Firmware 
--- drivers/platform/saf3000/fpgaf_info.c
+++ drivers/platform/saf3000/fpgaf_info.c
@@ -40,40 +40,12 @@
 #include <linux/leds.h>
 #include <sysdev/fsl_soc.h>
 #include <saf3000/saf3000.h>
+#include <saf3000/fpgaf.h>
 
 /*
  * driver FPGAF INFO
  */
  
-struct fpgaf {
-	u16 ident;
-	u16 version;
-	u16 res1[6];
-	u16 reset;
-	u16 res2[7];
-	u16 it_mask;
-	u16 it_pend;
-	u16 it_ack;
-	u16 it_ctr;
-	u16 res3[4];
-	u16 alrm_in;
-	u16 alrm_out;
-	u16 res4[6];
-	u16 fonc_gen;
-	u16 addr;
-	u16 res5[6];
-	u16 pll_ctr;
-	u16 pll_status;
-	u16 pll_src;
-	u16 res6[5];
-	u16 net_ref;
-	u16 etat_ref;
-	u16 res7[6];
-	u16 syn_h110;
-	u16 res8[7];
-	u16 test;
-};
-
 struct fpgaf_info_data {
 	struct fpgaf __iomem *fpgaf;
 	u16 *mezz;
