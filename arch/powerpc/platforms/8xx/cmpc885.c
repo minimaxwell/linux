da97b3a70ac94eeb62f92f89f9f852be676a3e09
Florent TRINH-THAI <florent.trinh-thai@c-s.fr>
2012-01-16 10:06:17 +0000

    - Il n'y a plus d'init faite dans le noyau. Attention necessite BTL 3.5.0 minimum.

--- arch/powerpc/platforms/8xx/cmpc885.c
+++ arch/powerpc/platforms/8xx/cmpc885.c
@@ -27,95 +27,6 @@ struct cpm_pin {
 	int port, pin, flags;
 };
 
-static struct cpm_pin cmpc885_pins[] = {
-	/* SMC1 */
-	{CPM_PORTB, 24, CPM_PIN_INPUT}, 					/* RX  port DEBUG	*/
-	{CPM_PORTB, 25, CPM_PIN_INPUT}, 					/* TX  port DEBUG	*/
-
-	/* SMC2 */
-	{CPM_PORTE, 20, CPM_PIN_INPUT | CPM_PIN_SECONDARY},			/* SMTXD2		*/
-	{CPM_PORTB, 20, CPM_PIN_INPUT}, 					/* SMRXD2		*/
-	
-	/* SCC2 */
-	{CPM_PORTA, 12, CPM_PIN_INPUT}, 					/* TXD2			*/
-	{CPM_PORTA, 13, CPM_PIN_INPUT}, 					/* RXD2			*/
-	{CPM_PORTB, 18, CPM_PIN_OUTPUT}, 					/* RTS2			*/
-	{CPM_PORTC,  9, CPM_PIN_INPUT | CPM_PIN_GPIO | CPM_PIN_SECONDARY}, 	/* CTS2			*/
-	{CPM_PORTC,  8, CPM_PIN_INPUT | CPM_PIN_GPIO | CPM_PIN_SECONDARY}, 	/* CD2			*/
-	
-	/* SCC3 */
-	{CPM_PORTD, 10, CPM_PIN_INPUT}, 					/* TXD3			*/
-	{CPM_PORTD, 11, CPM_PIN_INPUT}, 					/* RXD3			*/
-	{CPM_PORTD,  7, CPM_PIN_INPUT}, 					/* RTS3			*/
-	{CPM_PORTC,  5, CPM_PIN_INPUT | CPM_PIN_GPIO | CPM_PIN_SECONDARY}, 	/* CTS3			*/
-	{CPM_PORTC,  4, CPM_PIN_INPUT | CPM_PIN_GPIO | CPM_PIN_SECONDARY}, 	/* CD3			*/
-	
-	/* SCC4 */
-	{CPM_PORTD,  9, CPM_PIN_OUTPUT}, 					/* TXD4			*/
-	{CPM_PORTE, 25, CPM_PIN_INPUT}, 					/* RXD4			*/
-	
-	/* SPI */
-	{CPM_PORTB, 28, CPM_PIN_OUTPUT},					/* MISO			*/
-	{CPM_PORTB, 29, CPM_PIN_OUTPUT},					/* MOSI 		*/
-	{CPM_PORTB, 30, CPM_PIN_OUTPUT},					/* CLK			*/
-	{CPM_PORTB, 14, CPM_PIN_OUTPUT | CPM_PIN_GPIO},				/* CS_TEMP		*/
-	{CPM_PORTB, 19, CPM_PIN_OUTPUT | CPM_PIN_GPIO},				/* WR_TEMP		*/
-	{CPM_PORTB, 21, CPM_PIN_OUTPUT | CPM_PIN_GPIO},				/* CS_EEPROM		*/
-	
-	/* NAND */
-	{CPM_PORTD, 12, CPM_PIN_OUTPUT | CPM_PIN_GPIO},				/* CLE_DISK		*/
-	{CPM_PORTD, 13, CPM_PIN_OUTPUT | CPM_PIN_GPIO},				/* ALE_DISK 		*/
-	{CPM_PORTD, 15, CPM_PIN_OUTPUT | CPM_PIN_GPIO},				/* CS_DISK		*/
-
-	/* USB */
-	{CPM_PORTC,  7, CPM_PIN_OUTPUT},					/* USB_TXN			*/ 
-	{CPM_PORTC,  6, CPM_PIN_OUTPUT},					/* USB_TXN			*/ 
-	{CPM_PORTA, 14, CPM_PIN_INPUT},						/* USB_TXN			*/ 
-	{CPM_PORTC, 11, CPM_PIN_INPUT | CPM_PIN_SECONDARY},			/* USB_TXN			*/ 
-	{CPM_PORTC, 10, CPM_PIN_INPUT | CPM_PIN_SECONDARY},			/* USB_TXN			*/ 
-	{CPM_PORTA, 15, CPM_PIN_INPUT},						/* USB_TXN			*/ 
-		
-	/* MII1 */
-	{CPM_PORTA, 0, CPM_PIN_INPUT},						/* RMII-1_RXD1		*/
-	{CPM_PORTA, 1, CPM_PIN_INPUT},						/* RMII-1_RXD0		*/
-	{CPM_PORTA, 2, CPM_PIN_INPUT},						/* RMII-1_CRS_DV	*/
-	{CPM_PORTA, 3, CPM_PIN_INPUT},						/* RMII-1_RXER		*/
-	{CPM_PORTA, 4, CPM_PIN_OUTPUT},						/* RMII-1_TXD1		*/
-	{CPM_PORTA, 11, CPM_PIN_OUTPUT},					/* RMII-1_TXD0		*/
-	{CPM_PORTB, 31, CPM_PIN_INPUT},						/* RMII-1_REF_CLK	*/
-	
-	{CPM_PORTD, 8, CPM_PIN_INPUT},						/* R_MDC		*/
-
-	/* MII2 */
-	{CPM_PORTE, 14, CPM_PIN_OUTPUT | CPM_PIN_SECONDARY},			/* RMII-2_TXD0		*/
-	{CPM_PORTE, 15, CPM_PIN_OUTPUT | CPM_PIN_SECONDARY},			/* RMII-2_TXD1		*/
-	{CPM_PORTE, 16, CPM_PIN_OUTPUT},					/* RMII-2_REF_CLK	*/
-	{CPM_PORTE, 19, CPM_PIN_OUTPUT | CPM_PIN_SECONDARY},			/* RMII-2_TXEN		*/
-	{CPM_PORTE, 21, CPM_PIN_OUTPUT},					/* RMII-2_RXD0		*/
-	{CPM_PORTE, 22, CPM_PIN_OUTPUT},					/* RMII-2_RXD1		*/
-	{CPM_PORTE, 26, CPM_PIN_OUTPUT},					/* RMII-2_CRS_DV	*/
-	{CPM_PORTE, 27, CPM_PIN_OUTPUT},					/* RMII-2_RXER		*/
-};
-
-static void __init init_ioports(void)
-{
-	int i;
-
-	for (i = 0; i < ARRAY_SIZE(cmpc885_pins); i++) {
-		struct cpm_pin *pin = &cmpc885_pins[i];
-		cpm1_set_pin(pin->port, pin->pin, pin->flags);
-	}
-
-	cpm1_clk_setup(CPM_CLK_SCC2, CPM_BRG1, CPM_CLK_RTX);
-	cpm1_clk_setup(CPM_CLK_SCC3, CPM_BRG2, CPM_CLK_RTX);
-	cpm1_clk_setup(CPM_CLK_SCC4, CPM_BRG3, CPM_CLK_RTX);
-	cpm1_clk_setup(CPM_CLK_SMC2, CPM_BRG3, CPM_CLK_RTX); /* SMC2 sera cadence plus loin par un BRG dans le FPGA via CLK5 */
-	
-	cpm1_clk_setup(CPM_CLK_SMC1, CPM_BRG4, CPM_CLK_RTX);
-	
-	/* Set FEC1 and FEC2 to RMII mode */
-	mpc8xx_immr->im_cpm.cp_cptr = 0x00000180;
-}
 
 static struct cpm_pin cmpc885_pins_fev170[] = { /* FEV 170, a supprimer une fois FEV 169 traitee */
 	{CPM_PORTC,  9, CPM_PIN_INPUT | CPM_PIN_GPIO }, 	/* CTS2			*/
@@ -178,7 +89,8 @@ arch_initcall(mpc8xx_early_ping_watchdog);
 static void __init cmpc885_setup_arch(void)
 {
 	cpm_reset();
-	init_ioports();
+	/* Set FEC1 and FEC2 to RMII mode on 100Mbps speed */
+	mpc8xx_immr->im_cpm.cp_cptr = 0x00000180;
 	mpc8xx_early_ping_watchdog();
 }
 
