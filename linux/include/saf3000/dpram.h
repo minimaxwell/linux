31b42532914f0f8c46e672d11b97c10d7056beb7
Patrick VASSEUR <patrick.vasseur@c-s.fr>
2013-04-05 07:27:54 +0000

    [#26723#csoip] Ajout dialogue bouclage aerien

--- linux/include/saf3000/dpram.h
+++ linux/include/saf3000/dpram.h
@@ -48,7 +48,8 @@ extern int write_fifo_cmde(short *cmde);
 #define MSG_MANAGE			32	/* Msg 32 = message module MANAGE */
 #define MSG_CAS				33	/* Msg 33 = message module CAS */
 #define MSG_BSS				34	/* Msg 34 = message module BSS/GRS */
+#define MSG_DAV_EM			35	/* Msg 35 = message module DAV_EM/GRS */
 	
-#define MSG_LAST_MSG			35
+#define MSG_LAST_MSG			36
 
 #endif /* __DPRAM_H__ */
