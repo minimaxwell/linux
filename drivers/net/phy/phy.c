aa1a0ab260a1109e9d93f555d5470745d566e93a
Marc BALEMBOY <marc.balemboy@c-s.fr>
2015-10-27 15:50:46 +0000

    [#109679] KNLD: Basculement eth1-B -> eth1-A trop lent (> 200ms) - mise a jour du link dans l'etat PHY_DOUBLE_ATTACHEMENT pour basculer plus vite

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -897,6 +897,16 @@ void phy_state_machine(struct work_struct *work)
 		}
 		break;
 	case PHY_DOUBLE_ATTACHEMENT:
+		err = phy_read_status(phydev);
+
+		if (err)
+			break;
+
+		if (!phydev->link) {
+			phydev->state = PHY_NOLINK;
+			break;
+		}
+
 		/* Only register a CHANGE if we are
 		 * polling or ignoring interrupts
 		 */
