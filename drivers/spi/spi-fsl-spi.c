76aca4e2cd6809f621daf2b717952c76efb8d7d7
Christophe LEROY <christophe.leroy@c-s.fr>
2016-06-09 07:49:48 +0000

    Il ne faut pas signaler que le transfer est fini avant d'avoir envoye la post-sequence vers le codec (mode 'maison' SPI_TROLL qui envoie 4 coups d'horloge avec le chipselect inactif, mode necessaire pour les codecs de MIAE et MCR2G) sinon un autre transfert peut demarrer et ecraser les BDs de la derniere sequence
    [#115423]

--- drivers/spi/spi-fsl-spi.c
+++ drivers/spi/spi-fsl-spi.c
@@ -409,7 +409,6 @@ static int fsl_spi_do_one_msg(struct spi_master *master,
 	}
 
 	m->status = status;
-	spi_finalize_current_message(master);
 
 	if (status || !cs_change) {
 		ndelay(nsecs);
@@ -422,6 +421,7 @@ static int fsl_spi_do_one_msg(struct spi_master *master,
 		status = fsl_spi_setup_transfer(spi, &t);
 		status = fsl_spi_bufs(spi, &t, 0);
 	}
+	spi_finalize_current_message(master);
 
 	fsl_spi_setup_transfer(spi, NULL);
 	return 0;
