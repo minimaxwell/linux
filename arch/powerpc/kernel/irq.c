f0b1e083423f3fa9f833c6e781dc55a174d72573
Christophe LEROY <christophe.leroy@c-s.fr>
2017-12-07 17:01:03 +0000

    Revert "powerpc: handle simultaneous interrupts at once"
    This reverts commit 83d20eab578630c6125508ff243ffacd9bffa137.
    
    [#130033]

--- arch/powerpc/kernel/irq.c
+++ arch/powerpc/kernel/irq.c
@@ -522,11 +522,7 @@ void __do_irq(struct pt_regs *regs)
 	if (unlikely(!irq))
 		__this_cpu_inc(irq_stat.spurious_irqs);
 	else
-		do {
-			generic_handle_irq(irq);
-
-			irq = ppc_md.get_irq();
-		} while (irq);
+		generic_handle_irq(irq);
 
 	trace_irq_exit(regs);
 
