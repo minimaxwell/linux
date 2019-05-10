5e8cd86ac760637d03cbf0765fb7fafc2039a19b
Christophe Leroy <christophe.leroy@c-s.fr>
2019-05-10 14:28:59 +0000

    [#148796] Suppression de fichiers qui ne sont pas dans le noyau officiel et qui sont restes au fur et a mesure des mises a jour

--- tools/perf/config/feature-checks/test-glibc.d
+++
@@ -1,5 +0,0 @@
-test-glibc.bin: test-glibc.c /opt/cldk/usr/include/stdc-predef.h \
- /opt/cldk/usr/include/gnu/libc-version.h \
- /opt/cldk/usr/include/features.h /opt/cldk/usr/include/sys/cdefs.h \
- /opt/cldk/usr/include/bits/wordsize.h /opt/cldk/usr/include/gnu/stubs.h \
- /opt/cldk/usr/include/gnu/stubs-32.h
