dc3ec2161a924f4ede4a6a6dda7e35dc9315a771
Maxime Chevallier <maxime.chevallier@bootlin.com>
2023-10-31 10:18:00 -0700

    net: phy: Introduce ethernet link topology representation
    Link topologies containing multiple network PHYs attached to the same
    net_device can be found when using a PHY as a media converter for use
    with an SFP connector, on which an SFP transceiver containing a PHY can
    be used.
    
    With the current model, the transceiver's PHY can't be used for
    operations such as cable testing, timestamping, macsec offload, etc.
    
    The reason being that most of the logic for these configuration, coming
    from either ethtool netlink or ioctls tend to use netdev->phydev, which
    in multi-phy systems will reference the PHY closest to the MAC.
    
    Introduce a numbering scheme allowing to enumerate PHY devices that
    belong to any netdev, which can in turn allow userspace to take more
    precise decisions with regard to each PHY's configuration.
    
    The numbering is maintained per-netdev, in a phy_device_list.
    The numbering works similarly to a netdevice's ifindex, with
    identifiers that are only recycled once INT_MAX has been reached.
    
    This prevents races that could occur between PHY listing and SFP
    transceiver removal/insertion.
    
    The identifiers are assigned at phy_attach time, as the numbering
    depends on the netdevice the phy is attached to. The PHY index can be
    re-used for PHYs that are persistent.
    
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -153,6 +153,7 @@
 #include <linux/prandom.h>
 #include <linux/once_lite.h>
 #include <net/netdev_rx_queue.h>
+#include <linux/phy_link_topology_core.h>
 
 #include "dev.h"
 #include "net-sysfs.h"
@@ -10875,6 +10876,12 @@ struct net_device *alloc_netdev_mqs(int sizeof_priv, const char *name,
 #ifdef CONFIG_NET_SCHED
 	hash_init(dev->qdisc_hash);
 #endif
+	dev->link_topo = phy_link_topo_create(dev);
+	if (IS_ERR(dev->link_topo)) {
+		dev->link_topo = NULL;
+		goto free_all;
+	}
+
 	dev->priv_flags = IFF_XMIT_DST_RELEASE | IFF_XMIT_DST_RELEASE_PERM;
 	setup(dev);
 
@@ -10962,6 +10969,8 @@ void free_netdev(struct net_device *dev)
 	dev->core_stats = NULL;
 	free_percpu(dev->xdp_bulkq);
 	dev->xdp_bulkq = NULL;
+
+	phy_link_topo_destroy(dev->link_topo);
 
 	/*  Compatibility with error handling in drivers */
 	if (dev->reg_state == NETREG_UNINITIALIZED) {
