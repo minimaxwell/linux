b4952f4fafddee1e8b64c2a4c67a770a7add0fd4
Maxime Chevallier <maxime.chevallier@bootlin.com>
2024-10-16 08:47:31 +0200

    net: phy: add missing include
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -25,6 +25,7 @@
 #include <linux/ethtool_netlink.h>
 #include <linux/phy.h>
 #include <linux/phy_led_triggers.h>
+#include <linux/phy_port.h>
 #include <linux/sfp.h>
 #include <linux/workqueue.h>
 #include <linux/mdio.h>
