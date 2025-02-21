1b953d28d62b93c4ead2fd52ec3fec66597a2296
Maxime Chevallier <maxime.chevallier@bootlin.com>
2025-02-21 17:38:52 +0100

    net: phy: drop phy_settings and the associated lookup helpers
    The phy_settings array is no longer relevant as it has now been replaced
    by the link_caps array and associated phy_caps helpers.
    
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -1320,18 +1320,6 @@ const char *phy_rate_matching_to_str(int rate_matching);
 
 int phy_interface_num_ports(phy_interface_t interface);
 
-/* A structure for mapping a particular speed and duplex
- * combination to a particular SUPPORTED and ADVERTISED value
- */
-struct phy_setting {
-	u32 speed;
-	u8 duplex;
-	u8 bit;
-};
-
-const struct phy_setting *
-phy_lookup_setting(int speed, int duplex, const unsigned long *mask,
-		   bool exact);
 void of_set_phy_supported(struct phy_device *phydev);
 void of_set_phy_eee_broken(struct phy_device *phydev);
 void of_set_phy_timing_role(struct phy_device *phydev);
